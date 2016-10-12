40a8b00f83389f89d538d19be079f464ffa8d686
root <root@wizdata-centos64-caravel.csbacloud.internal>
2016-10-12 14:12:26 +0800

    add max page check

--- ccopyrightSpider/spiders/cCopyRightSpider.py
+++ ccopyrightSpider/spiders/cCopyRightSpider.py
@@ -1,16 +1,21 @@
 #!/usr/bin/env pythonmZ
 # -*- coding: utf-8 -*-
 #import pdb
+import re
 import scrapy
 from ccopyrightSpider.items import CCopyRightItem
 from pyquery import PyQuery as pq
  
 class CCopyRightSpider(scrapy.Spider):
-    name = 'CatchCCopyRight'
+    name = 'catchCCRofSh'
     allowed_domains = ['ccopyright.com.cn']
     start_urls = [
-            "http://www.ccopyright.com.cn/cpcc/RRegisterAction.do?method=list&no=fck&sql_name=&sql_regnum=&sql_author=%C9%CF%BA%A3&curPage=1"
+            "http://www.ccopyright.com.cn"
     ]
+
+    def printCatchPercent(self, pageIndex, startPageIndex, endPageIndex):
+        print "========>> PageIndex of Shanghai CopyRight:["+ str(pageIndex) + "]  From Page "+str(startPageIndex)+" to Page "+str(endPageIndex - 1)+" [ "+ str((pageIndex - startPageIndex)*100/(endPageIndex - startPageIndex))  +" Percent Finished ]"
+        
 
     def parse(self, response):
         startPageIndex = 0
@@ -23,20 +28,28 @@ class CCopyRightSpider(scrapy.Spider):
                 endPageIndex = int(line) + 1
         pageRange.close()
 
+        maxPageIndex = 0
         for pageIndex in range(startPageIndex, endPageIndex):
-	    #print str(pageIndex-startPageIndex)
-	    #print str(endPageIndex - startPageIndex)
-            print "PageIndex of Shanghai CopyRight:["+ str(pageIndex) + "]  From Page "+str(startPageIndex)+" to Page "+str(endPageIndex - 1)+" [ "+ str((pageIndex - startPageIndex)*100/(endPageIndex - startPageIndex))  +" Percent Finished ]"
-            
+            if maxPageIndex == 0:
+                pattern = re.compile("\/共(\d*)页")
+                res = pattern.search(doc(u'form[name="generalForm"] table:eq(2) td:eq(0)').text()).groups()
+                print res[0]
+                maxPageIndex = int(res[0])
+            else:
+                if pageIndex > maxPageIndex:
+                    print "====>Reach Max Page : BREAK"
+                    break
+            self.printCatchPercent(pageIndex, startPageIndex, endPageIndex)
             doc = pq(
                 "http://www.ccopyright.com.cn/cpcc/RRegisterAction.do?method=list&no=fck&sql_name=&sql_regnum=&sql_author=%C9%CF%BA%A3&curPage="+str(pageIndex)
             )
             #pdb.set_trace()
-            id = 1
+            d = 1
             for i in range(10):
                 id+=1;
                 item = CCopyRightItem()
-                print "getScript: doc(u'form[name=""generalForm""] table:eq(1) tr:eq("+str(id)+") td:eq(0)').text()"
+                if len(doc(u'form[name="generalForm"] table:eq(1) tr:eq('+str(id)+') td:eq(0)').text())==0:
+                    continue
                 item['number'] = doc(u'form[name="generalForm"] table:eq(1) tr:eq('+str(id)+') td:eq(0)').text()
                 item['type'] = doc(u'form[name="generalForm"] table:eq(1) tr:eq('+str(id)+') td:eq(1)').text()
                 item['name'] = doc(u'form[name="generalForm"] table:eq(1) tr:eq('+str(id)+') td:eq(2)').text()
